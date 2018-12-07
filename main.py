ecfd27447ffca6b4cbdc20caf4d565371de0a839
SalwaAhmedfci <37433010+SalwaAhmedfci@users.noreply.github.com>
2018-12-07 11:22:23 -0500

    reading from files

--- main.py
+++ main.py
@@ -1,44 +1,82 @@
-#!/usr/bin/env python3
-
 import csv
 
+import array
 
 # function to find if
 # given point lies inside
 # a given rectangle or not.
+import csv
 
 
-def FindPoint(x1, y1, x2,
-              y2, x, y):
-    if ((x > 1 or x == 1) and (x < 5 or x == 5) and
-            (y > 1 or y == 1) and (y < 6 or y == 6)):
-        return "New York"
-    if ((x > 10 or x == 10) and (x < 13 or x == 13) and
-                (y > 1 or y == 1) and (y < 2 or y == 2)):
-        return "Paris"
-    if ((x > 100 or x == 100) and (x < 78 or x == 78) and
-                (y > 110 or y == 110) and (y < 82or y == 82)):
-        return "London"
+
+
+def checking_Points(x,y,x1,y1,x2,y2,cityName):
+    result = []
+    res = ["None"]
+    if ((x >= x1 and x <= x2) or (x <= x1 and x >= x2)):
+        inside_x = True
     else:
-        return "the point is outside all"
+        inside_x = False
+    if ((y >= y1 and y <= y2) or (y <= y1 and y >= y2)):
+        inside_y = True
+    else:
+        inside_y = False
+
+    if (inside_x == True and inside_y == True) :
+        return True
+    else:
+        return False
 
 
-# Driver code
+
+
+
+
+
+
 
 if __name__ == "__main__":
+    # initializing_lists
+    cities_List = [];
+    pointList = [];
+    result = [];
+    # reading from files
 
-    # bttom-left and top-right
-    # corners of rectangle.
-    # use multiple assigment
-    x1, y1, x2, y2 = 1, 1, 5, 6
-
-    # given point
-    x, y = 2, 1
-
-    # function call
-
-    print( FindPoint(x1, y1, x2,
-                 y2, x, y))
+    with open('points.csv', 'r') as point_file:
+        points = csv.reader(point_file)
+        for row in points:
+            pointList.append(row)
+        points2check = pointList[1:]
 
 
 
+    with open('cities.csv', 'r') as cities_file:
+        cities = csv.reader(cities_file)
+        for row in cities:
+            cities_List.append(row)
+        cities = cities_List[1:]
+
+
+
+
+
+    listofcities = []
+
+    for point in points2check:
+        cityNameFinal = "None"
+
+        x = int(point[1])
+        y = int(point[2])
+
+        for city in cities:
+            cityName = city[0]
+            x1 = int(city[1])
+            y1 = int(city[2])
+            x2 = int(city[3])
+            y2 = int(city[4])
+            if (checking_Points(x, y, x1, y1, x2, y2, cityName)==True):
+                cityNameFinal = cityName;
+                break
+
+        result.append(checking_Points(x, y, x1, y1, x2, y2, cityNameFinal))
+        print(cityNameFinal)
